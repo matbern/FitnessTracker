5bfd5ddbf1a5b938b3e37752c30040263136268b
matbern <matbern@student.chalmers.se>
2014-05-21 13:34:03 +0200

    small fix

--- FitnessTracker/src/se/chalmers/fitnesstracker/GoalFragment.java
+++ FitnessTracker/src/se/chalmers/fitnesstracker/GoalFragment.java
@@ -6,7 +6,7 @@ import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
